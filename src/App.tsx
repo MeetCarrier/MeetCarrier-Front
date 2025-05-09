efe4284c7a8da83beb0fee74ef3ea71f1203266a
jsy0605 <jungsy0605@naver.com>
2025-05-09 14:55:47 +0900

    ✨로그인 페이지 추가, 메인 페이지 url /main으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import SurveyQ from "./pages/SurveyQ";
 import RangeSetting from "./pages/RangeSetting";
 import ChatListPage from "./pages/Chat/ChatListPage";
 import SurveyPage from "./pages/Chat/SurveyPage";
+import Login from "./pages/Login";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Utils/store";
 
@@ -17,13 +18,15 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
+            <Route index element={<Navigate to="/main" replace />} />
+            <Route path="main" element={<MainPage />} />
             <Route path="Hobby" element={<Hobby />} />
             <Route path="SurveyQuestion" element={<SurveyQ />} />
             <Route path="MeetCenter" element={<MeetCenter />} />
             <Route path="RangeSetting" element={<RangeSetting />} />
             <Route path="chat" element={<ChatListPage />} />
             <Route path="survey/:surveyId" element={<SurveyPage />} />
+            <Route path="Login" element={<Login />} />
           </Route>
         </Routes>
       </BrowserRouter>
