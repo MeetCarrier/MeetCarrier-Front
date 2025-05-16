6e472736035ffc46d4dc3472c1ca542ba8978a21
dsky03 <dsky03@naver.com>
2025-05-16 18:13:40 +0900

    ✅ 일기, 달력 페이지 추가
    일기, 달력 페이지 기능 X, 디자인 배치만

---
+++ src/pages/Calendar/Diary.tsx
@@ -0,0 +1,54 @@
+import NavBar from '../../components/NavBar';
+import questionmark_icon from '../../assets/img/icons/Dairy/questionmark_icon.svg';
+import btn1 from '../../assets/img/button/btn1.webp';
+
+function Dairy() {
+  return (
+    <>
+      <NavBar />
+
+      <div className="absolute top-[50px] text-[#333333] left-0 right-0 px-6 text-center">
+        <p className="text-[20px] font-MuseumClassic_L italic">
+          칭찬 일기 작성하기
+        </p>
+        <img
+          src={questionmark_icon}
+          alt="bell_default"
+          className="absolute top-1/2 -translate-y-1/2 right-6 w-[20px] h-[20px]"
+        />
+      </div>
+
+      <div className="flex flex-col w-full h-[calc(100%-200px)] px-4">
+        <div className="flex justify-center mb-3">
+          <div className="w-[200px] h-[60px] bg-white" />
+        </div>
+
+        <div className="w-full max-w-md mx-auto flex flex-col flex-1">
+          <div className="text-left mb-3">
+            <p className="text-[16px] font-semibold">오늘의 나를 칭찬하자면?</p>
+            <p className="text-[14px] text-[#999]">2025.03.29</p>
+          </div>
+
+          {/* textarea가 나머지 공간 전부 차지 */}
+          <textarea
+            placeholder="입력해주세요..."
+            className="w-full flex-1 p-3 border border-[#D9D9D9] rounded-[10px] resize-none text-[14px] bg-white"
+          />
+          <div className="text-right text-[12px] text-[#999] mt-1 mb-4">
+            0 / 100
+          </div>
+
+          {/* 버튼 */}
+          <button className="relative w-full mb-3">
+            <img src={btn1} alt="버튼1" className="w-full" />
+            <span className="absolute inset-0 flex items-center justify-center font-GanwonEduAll_Bold cursor-pointer">
+              다음
+            </span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Dairy;
